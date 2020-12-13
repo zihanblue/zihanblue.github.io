ab540442a1312547db92bbe4c5bfe170f7a4b5e9
zihanblue <70513631+zihanblue@users.noreply.github.com>
2020-12-14 02:54:53 +0800

    Add files via upload

---
+++ final/selector.js
@@ -0,0 +1,5 @@
+function table(){
+    var selecter = parent.document.getElementById("selecter");
+                                if (selecter.value=="0"){document.getElementById("iframe").src="https://airtable.com/shrwJtrzdVKpZThYS"};
+                                if (selecter.value=="1"){document.getElementById("iframe").src="https://airtable.com/embed/shrKzsA5a5S0L7IBy?backgroundColor=blue&viewControls=on"};
+                                if (selecter.value=="2"){document.getElementById("iframe").src="https://airtable.com/embed/shrWlMl0oxo69iaSj?backgroundColor=blue&viewControls=on"};}
---
+++ final/小岛总览.js
@@ -0,0 +1,5 @@
+function table(){
+    var selecter = parent.document.getElementById("selecter");
+                                if (selecter.value=="0"){document.getElementById("iframe").src="https://airtable.com/embed/shrZ8an4gXYHhXLJa?backgroundColor=pink&viewControls=on"};
+                                if (selecter.value=="1"){document.getElementById("iframe").src="https://airtable.com/embed/shrwJtrzdVKpZThYS?backgroundColor=pink&viewControls=on"};
+                                if (selecter.value=="2"){document.getElementById("iframe").src="https://airtable.com/embed/shrenqHjMpymDcdFl?backgroundColor=pink&viewControls=on"};}
